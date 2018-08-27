0188958808f4c3e90b1c22ae3ea3cf1bb875d091
مهندس شهرام <komeilipour@payampardaz.local>
2018-08-27 19:16:28 +0430

    change document place

--- TestCPP/GeeksForGeeks/HackerRankSampleCodes.cpp
+++ TestCPP/GeeksForGeeks/HackerRankSampleCodes.cpp
@@ -802,8 +802,68 @@ vector<string> split_string(string input_string) {
 	return splits;
 }
 
+class Palindrom {
 
-int main()
+private:
+	vector<char> stack = {};
+	vector<char> queue = {};
+	int stackCursor = 0;
+	int queueCursor = 0;
+public:
+	void pushCharacter(char c) {
+		stack.push_back(c);
+	}
+
+	void enqueueCharacter(char c) {
+		queue.push_back(c);
+		queueCursor++;
+	}
+
+	char popCharacter() {
+		return stack[stackCursor];
+		stackCursor++;
+	}
+	char dequeueCharacter() {
+		return queue[queueCursor - 1];
+		queueCursor - 1;
+	}
+};
+
+int mainPalindrom()
 {
+	// read the string s.
+	string s;
+	getline(cin, s);
+
+	// create the Solution class object p.
+	Palindrom obj;
+
+	// push/enqueue all the characters of string s to stack.
+	for (int i = 0; i < s.length(); i++) {
+		obj.pushCharacter(s[i]);
+		obj.enqueueCharacter(s[i]);
+	}
+
+	bool isPalindrome = true;
+
+	// pop the top character from stack.
+	// dequeue the first character from queue.
+	// compare both the characters.
+	for (int i = 0; i < s.length() / 2; i++) {
+		if (obj.popCharacter() != obj.dequeueCharacter()) {
+			isPalindrome = false;
+
+			break;
+		}
+	}
+
+	// finally print whether string s is palindrome or not.
+	if (isPalindrome) {
+		cout << "The word, " << s << ", is a palindrome.";
+	}
+	else {
+		cout << "The word, " << s << ", is not a palindrome.";
+	}
+
 	return 0;
 }
